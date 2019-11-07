a9f46201564b9010e137e37410d7298976bdfef3
Manuel Montenegro <montenegro@fdi.ucm.es>
2019-11-07 20:36:14 +0100

    Lexer tests

--- test/lexer_test.exs
+++ test/lexer_test.exs
@@ -3,6 +3,8 @@ defmodule LexerTest do
 
   import WhileParser.Lexer
 
+  @keywords ~w{function ret end true false in if then else while do begin}
+
   test "only whitespace" do
     assert {:ok, []} = lexer("")
     assert {:ok, []} = lexer("  \n \t")
@@ -21,5 +23,41 @@ defmodule LexerTest do
     assert {:ok, [{:identifier, 1, "X1"}]} = lexer("X1")
     assert {:ok, [{:identifier, 1, "_d23"}]} = lexer("_d23")
     assert {:error, 1, _} = lexer("3d")
+  end
+
+  test "symbols: parentheses" do
+    assert {:ok, [{:'(', _ }, {:')', _ }]} = lexer("( )")
+    assert {:ok, [{:'(', _ }, {:')', _ }]} = lexer("()")
+    assert {:ok, [{:')', _ }, {:'(', _ }]} = lexer(" )(")
+  end
+
+  test "some arithmetic expressions" do
+    assert {:ok, [{:identifier, _, "x" }, {:'+', _ }, {:integer, _, 1}]} = lexer("x + 1")
+    assert {:ok, [{:integer, _, 3 }, {:'-', _ }, {:integer, _, 5}]} = lexer("3 -5")
+    assert {:ok, [{:integer, _, 3 }, {:'-', _ }, {:integer, _, 5}]} = lexer("3- 5")
+    assert {:ok, [{:identifier, _, "x"}, {:'<=', _}, {:integer, _, 1}]} = lexer("x<=1")
+    assert {:ok, [{:identifier, _, "x"}, {:'<=', _}, {:identifier, _, "y"}]} = lexer("x <=y")
+    assert {:ok, [{:identifier, _, "x"}, {:':=', _}, {:identifier, _, "x"}, {:'+', _}, {:integer, _, 10}]} = lexer("x := x + 10")
+  end
+
+  test "separate keywords" do
+    for k <- @keywords do
+      expected_atom = String.to_atom(k)
+      assert {:ok, [{^expected_atom, _}]} = lexer(k)
+    end
+  end
+
+  test "keywords by pairs" do
+    for k1 <- @keywords, k2 <- @keywords do
+      expected_atom_1 = String.to_atom(k1)
+      expected_atom_2 = String.to_atom(k2)
+      assert {:ok, [{^expected_atom_1, _}, {^expected_atom_2, _}]} = lexer("#{k1} #{k2}")
+    end
+  end
+
+  test "keywords by pairs without space" do
+    for k1 <- @keywords, k2 <- @keywords do
+      assert {:ok, [{:identifier, _, _}]} = lexer("#{k1}#{k2}")
+    end
   end
 end
