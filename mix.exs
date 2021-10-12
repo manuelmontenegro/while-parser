d8c0d6bffe978c46f10be13b3d5b4371d9e319d7
Manuel Montenegro <mmontene@ucm.es>
2021-10-12 18:50:51 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WhileParser.MixProject do
   def project do
     [
       app: :while_parser,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +21,8 @@ defmodule WhileParser.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.1"},
-      {:ex_doc, "~> 0.21.2", only: :dev, runtime: false}
+      {:jason, "~> 1.2"},
+      {:ex_doc, "~> 0.25.2", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
